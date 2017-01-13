6dc3006868a9b04c48d70dc7cfaab88820c55b8f
Andrew Maney <amaney@usc.edu>
2017-01-12 20:21:36 -0800

    bios section

--- js/effects.js
+++ js/effects.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
   var animateTime = 500;
 
+  //Class height animate
   $('.class').click(function() {
     // $(this).find('.spec').fadeIn();
     var spec = $(this).find('.spec');
     if(spec.height() === 0){
-      // autoAnimateSize(spec, animateTime);
       autoWidthAnimate(spec, animateTime);
-      // autoHeightAnimate(spec, animateTime);
       $(this).addClass('selected');
     } else {
       spec.animate({ height: '0' }, animateTime, function() {
@@ -17,11 +16,23 @@ $(document).ready(function() {
     }
   });
 
+  // Form display function
   $('#form .btn').click(function() {
     $(this).fadeOut(350);
     $('#form iframe').fadeIn(1800);
   });
 
+  $('.about').click(function() {
+    var about = $(this);
+    about.toggleClass('selected');
+    if(about.width() == 200) {
+      fullWidthAnimate($(this), animateTime);
+    } else {
+      about.animate({ width: '200' }, animateTime);
+    }
+  });
+
+  // Smooth scroll function
   $('a[href="#form"]:not([href="#"])').click(function() {
     $('#form .btn').hide();
     $('#form iframe').fadeIn(1800);
@@ -67,5 +78,11 @@ function autoWidthAnimate(element, time){
   element.animate({ width: autoWidth }, time, function() {
     autoHeightAnimate(element, time);
   });
-  // element.animate({ width: autoWidth }, time).promise().done(autoHeightAnimate(element,time));
+}
+
+function fullWidthAnimate(element, time) {
+  var curWidth = element.width(),
+      fullWidth = element.css('width', '100%').width();
+  element.width(curWidth);
+  element.animate({ width: fullWidth }, time);
 }
